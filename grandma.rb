4df8828272970d00b5ec27fb3c601cd4f49450d0
Shahwar Spall <shevy.spall@gmail.com>
2019-07-19 03:25:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,11 +14,15 @@
 
 
 def speak_to_grandma(phrase)
-if phrase.downcase == phrase
-  return "HUH?! SPEAK UP, SONNY!"
-elsif phrase == "I LOVE YOU GRANDMA!"
+
+if phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
+elsif phrase.downcase == phrase
+  return "HUH?! SPEAK UP, SONNY!"
+
+
 elsif phrase.upcase == phrase
+
   return "NO, NOT SINCE 1938!"
 
 
